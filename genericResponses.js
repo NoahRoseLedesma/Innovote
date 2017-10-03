da9bba6fb995eb1eb1a33f2d04894b551ac74a1e
noahroseledesma <noahroseledesma@seattleacademy.org>
2017-10-03 14:47:39 -0700

    Add bad request generic response

--- genericResponses.js
+++ genericResponses.js
@@ -7,5 +7,10 @@ module.exports = {
   forbidden: function(res)
   {
     res.status(401).json({"message":"You are not authorized to view this content or feature."}).end();
+  },
+
+  BadRequest: function(res)
+  {
+    res.status(400).json({"message":"The request could not be completed due to an invalid input."}).end();
   }
 }
